0a1567f5578fc3aca5811c24cb5d8587d640d75e
Francis Tan <francis.tan@ufinity.com>
2011-01-18 18:41:27 +0800

    Until 10.2.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
   # Show User ID, name and e-mail
   def show
     @user = User.find(params[:id])
@@ -22,6 +23,28 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit # Edit User Page
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+
+  def update #Update User in database
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
 
 end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,6 +15,10 @@ module SessionsHelper
   def signed_in?
     !current_user.nil?
   end
+  
+  def deny_access
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
 
   def sign_out
     cookies.delete(:remember_token)
