58b9b681d1341c95c8bd3a7badfe4c1fcb505140
Francis Tan <francis.tan@ufinity.com>
2011-01-24 12:05:44 +0800

    Until 12.1.1

---
+++ db/migrate/20110124040134_create_relationships.rb
@@ -0,0 +1,17 @@
+class CreateRelationships < ActiveRecord::Migration
+  def self.up
+    create_table :relationships do |t|
+      t.integer :follower_id
+      t.integer :followed_id
+
+      t.timestamps
+    end
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    add_index :relationships, [:follower_id, :followed_id], :unique => true # composite index that enforces uniqueness of pairs of (follower_id, followed_id), so that a user can’t follow another user more than once
+  end
+
+  def self.down
+    drop_table :relationships
+  end
+end
